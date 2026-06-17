8f96624ba168fd860cdb22c51aa7d8d569fe37e3
slesforever <sles.forever@gmail.com>
2026-06-17 17:50:27 +0800

    Update identitiesData.js

--- Functions/GameSystem/Pulls/identitiesData.js
+++ Functions/GameSystem/Pulls/identitiesData.js
@@ -349,10 +349,10 @@ const upTargets = {
     '0000':    
         [null],
     'Egos':    
-        ["[HE] 著影揮刀 / Shadow-Vested Bladesinger - 默爾索"],
+        [" [HE] 著影揮刀 / Shadow-Vested Bladesinger - 默爾索"],
     '000':     
-        ["[LCD現場推理小隊] 以實瑪麗 / LCD OSIR Team Ishmael"],
-        ["[S公司 推奴人] 鴻路 / S Corp. Ch'unokkun Hong Lu" ],
+        [" [LCD現場推理小隊] 以實瑪麗 / LCD OSIR Team Ishmael"],
+        [" [S公司 推奴人] 鴻路 / S Corp. Ch'unokkun Hong Lu" ],
     '00':      
         [null],
     '0':       
