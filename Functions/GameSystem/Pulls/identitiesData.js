2b780b759ca06e5efab0906f7fd3dbc9e79e0785
slesforever <sles.forever@gmail.com>
2026-06-18 13:48:56 +0800

    Update identitiesData.js

--- Functions/GameSystem/Pulls/identitiesData.js
+++ Functions/GameSystem/Pulls/identitiesData.js
@@ -348,9 +348,9 @@ const upTargets = {
         ["［漆黑噤默］羅蘭 / The Black Silence Roland"], // 修正稀有度對應標籤
     '0000':    
         [null],
-    'Egos':    
+    'Egos':
         [" [HE] 著影揮刀 / Shadow-Vested Bladesinger - 默爾索"],
-    '000':     
+    '000':
         [" [LCD現場推理小隊] 以實瑪麗 / LCD OSIR Team Ishmael"],
         [" [S公司 推奴人] 鴻路 / S Corp. Ch'unokkun Hong Lu" ],
     '00':      
