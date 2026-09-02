199ef32fe4b61ac867bdcae6796380b32fa7503a
slesforever <sles.forever@gmail.com>
2026-09-02 18:11:55 +0800

    feat: sync manual command deployment for shop and giveaways

--- Functions/deploy-commands.js
+++ Functions/deploy-commands.js
@@ -7,6 +7,7 @@ const commands = [
     new SlashCommandBuilder().setName('pull').setDescription('開啟狂氣提取介面'),
     new SlashCommandBuilder().setName('pack').setDescription('查看背包與資源'),
     new SlashCommandBuilder().setName('list').setDescription('查看卡池機率與清單'),
+    new SlashCommandBuilder().setName('limbusids').setDescription('查看角色 ID 與中英文名稱'),
     new SlashCommandBuilder().setName('battle').setDescription('選擇難度進入戰鬥'),
     new SlashCommandBuilder().setName('party').setDescription('查看與管理隊伍'),
     new SlashCommandBuilder().setName('sinner').setDescription('查看罪人資料'),
@@ -41,7 +42,13 @@ const commands = [
     new SlashCommandBuilder().setName('givefragments').setDescription('Sles專屬：發放碎片').setDefaultMemberPermissions(admin),
     new SlashCommandBuilder().setName('givescrolls').setDescription('Sles專屬：發放抽卡券').setDefaultMemberPermissions(admin),
     new SlashCommandBuilder().setName('givethreads').setDescription('Sles專屬：發放絲線').setDefaultMemberPermissions(admin),
-    new SlashCommandBuilder().setName('announce').setDescription('Sles專屬：全伺服器公告').setDefaultMemberPermissions(admin).addStringOption(o => o.setName('message').setDescription('內容').setRequired(true)),
+    new SlashCommandBuilder().setName('announce').setDescription('Sles專屬：全伺服器公告').setDefaultMemberPermissions(admin).addStringOption(o => o.setName('message').setDescription('內容').setRequired(true)).addRoleOption(o => o.setName('role').setDescription('要通知的身分組（選填）')),
+    new SlashCommandBuilder().setName('shop').setDescription('開啟商城 UI'),
+    new SlashCommandBuilder().setName('shop-add').setDescription('Sles專屬：上架商城商品').setDefaultMemberPermissions(admin).addStringOption(o => o.setName('name').setDescription('商品名稱').setRequired(true)).addStringOption(o => o.setName('info').setDescription('商品資訊')).addIntegerOption(o => o.setName('lightseeds').setDescription('LightSeeds 價格').setMinValue(0)).addIntegerOption(o => o.setName('starcoins').setDescription('StarCoins 價格').setMinValue(0)).addIntegerOption(o => o.setName('minlevel').setDescription('最低等級').setMinValue(0)).addIntegerOption(o => o.setName('stock').setDescription('庫存').setMinValue(1)),
+    new SlashCommandBuilder().setName('shop-remove').setDescription('Sles專屬：下架商城商品').setDefaultMemberPermissions(admin).addStringOption(o => o.setName('item_id').setDescription('商品 ID').setRequired(true)),
+    new SlashCommandBuilder().setName('shop-confirm').setDescription('Sles專屬：確認商城序號已交付').setDefaultMemberPermissions(admin).addStringOption(o => o.setName('code').setDescription('購買序號').setRequired(true)),
+    new SlashCommandBuilder().setName('giveaway-create').setDescription('建立抽獎活動').setDefaultMemberPermissions(admin).addStringOption(o => o.setName('prize_name').setDescription('獎品名稱').setRequired(true)).addIntegerOption(o => o.setName('winners').setDescription('抽出人數').setRequired(true).setMinValue(1).setMaxValue(20)).addStringOption(o => o.setName('prize_info').setDescription('獎品資訊')).addIntegerOption(o => o.setName('max_participants').setDescription('最多參加人數').setMinValue(1).setMaxValue(10000)).addIntegerOption(o => o.setName('min_level').setDescription('最低等級').setMinValue(0).setMaxValue(100)).addIntegerOption(o => o.setName('entry_lightseeds').setDescription('參加扣除 LightSeeds').setMinValue(0)).addIntegerOption(o => o.setName('entry_starcoins').setDescription('參加扣除 StarCoins').setMinValue(0)).addIntegerOption(o => o.setName('prize_lightseeds').setDescription('得獎發放 LightSeeds').setMinValue(0)).addIntegerOption(o => o.setName('prize_starcoins').setDescription('得獎發放 StarCoins').setMinValue(0)).addIntegerOption(o => o.setName('duration').setDescription('持續分鐘').setMinValue(1).setMaxValue(10080)),
+    new SlashCommandBuilder().setName('giveaway-end').setDescription('提前結束抽獎').setDefaultMemberPermissions(admin).addStringOption(o => o.setName('id').setDescription('抽獎 ID').setRequired(true)),
     new SlashCommandBuilder().setName('help').setDescription('顯示指令清單'),
 ].map(command => command.toJSON());
 
